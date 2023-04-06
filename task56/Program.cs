a444f9c6d46cef3913451342dc4dcec91230b2bf
pashqa <pashqevi4@gmail.com>
2023-04-06 21:14:16 +0700

    Task 56 complete

---
+++ task56/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int FindMinRow(int[,] array)
+{
+    int index = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i,j];
+        }
+        if ((i < 1))
+        {
+            result = sum;
+            index = i + 1;
+        }
+        else if (sum < result)
+        {
+            result = sum;
+            index = i + 1;
+        }
+    
+
+
+    }
+    return index;
+}
+
+int[,] matrix = new int[4, 4];
+FillArray(matrix);
+PrintArray(matrix);
+int x = FindMinRow(matrix);
+Console.WriteLine($"The least sum of elements is in the lane number {x}");
